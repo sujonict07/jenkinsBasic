9a4874fa27ae270c5e9343ba9570cc39046e6de1
Md. Ruhul Amin Sujon <sujonict07@gmail.com>
2018-09-23 16:55:34 +0600

    Create virtual_env_create.sh

---
+++ virtual_env_create.sh
@@ -0,0 +1,16 @@
+sudo yum makecache
+sudo yum install yum-utils
+sudo yum install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum makecache
+sudo yum install python34u python34u-pip
+
+# python3.4 -V
+# pip3.4 -V
+# for python 3.5 and pip 3.5
+# sudo yum install python35u python35u-pip
+# for python 3.5 and pip 3.5
+# sudo yum install python35u python35u-pip
+# for python 3.5 and pip 3.5
+# sudo yum install python36u python36u-pip
+# If you have multiple version of Python 3 installed, you can change it as follows:
+# alias python3=$(which python3.x)
