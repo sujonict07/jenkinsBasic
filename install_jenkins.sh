ed49e2d63eea636e975b060591ebb3b4bf0ec5b4
Md. Ruhul Amin Sujon <sujonict07@gmail.com>
2019-07-30 11:19:49 +0600

    Create install_jenkins.sh

---
+++ install_jenkins.sh
@@ -0,0 +1,8 @@
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
